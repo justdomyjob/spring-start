9c77fb93fc5f26055ec7055b971620872ecb5ee9
seok <whgustjr302@naver.com>
2024-01-08 23:03:10 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,24 +6,16 @@ import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
+//스프링 데이터 JPA가 SpringDataJpaMemberRepository 를 스프링 빈으로 자동 등록해준다
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-    @Bean
-    public MemberRepository memberRepository() {
-//      return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
